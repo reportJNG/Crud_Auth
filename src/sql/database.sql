63380402a601471d3294d31c920eb11e65b130aa
reportJNG <hamzaremali10@gmail.com>
2026-02-08 06:32:03 -0800

    setted up the backend

---
+++ src/sql/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+  CHECK (
+    length(password) >= 8
+    AND password !~ '[#$]'
+  )
+);
